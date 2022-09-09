d18e20f31fae3d2381145e41ebcdefbe3a621056
Oskari Peltonen <oskari.peltonen@outlook.com>
2022-09-09 07:35:18 +0300

    initial commit

---
+++ src/main/java/peltonen/oskari/techboard/TechboardApplication.java
@@ -0,0 +1,13 @@
+package peltonen.oskari.techboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechboardApplication.class, args);
+	}
+
+}
